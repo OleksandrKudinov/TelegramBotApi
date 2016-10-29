3dbdacf98a936aeedfa30d54390d7a6ac371094f
Oleksandr.Kudinov <oleksandr.kudinov.1993@gmail.com>
2016-10-29 17:40:07 +0300

    Added bot configuration;

---
+++ Telegram/Telegram.BotApi/BotConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace Telegram.BotApi
+{
+    public  class BotConfig
+    {
+        public string Token { get; set; }
+        public string BotName { get; set; }
+        public string Nickname { get; set; }
+    }
+}
