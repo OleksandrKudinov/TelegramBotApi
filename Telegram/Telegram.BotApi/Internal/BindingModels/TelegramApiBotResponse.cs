8682b05b5d19abdb56d11ebc4a7523a29077a5c0
Oleksandr.Kudinov <oleksandr.kudinov.1993@gmail.com>
2016-10-29 19:40:17 +0300

    Added TelegramApiBotResponse model;

---
+++ Telegram/Telegram.BotApi/Internal/BindingModels/TelegramApiBotResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Telegram.BotApi.Internal.BindingModels
+{
+    public class TelegramApiBotResponse
+    {
+        public string ok { get; set; }
+        public Update[] result { get; set; }
+    }
+}
